e0df8e15ff0105364009a38fcd6a0bf5d6617739
crsimpson5 <crsimpson5@gmail.com>
2019-04-13 12:36:27 -0500

    Updated package.json

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
 import "./style.css";
 
 const Header = props => {
   return (
     <Navbar bg="dark" variant="dark" expand="lg">
       <Navbar.Brand>Clicky Game</Navbar.Brand>
+      <Nav>
+        <Nav.Link href="https://github.com/crsimpson5/clicky-game">GitHub</Nav.Link>
+      </Nav>
       <div className="ml-auto text-light">Score: {props.score}</div>
     </Navbar>
   );
