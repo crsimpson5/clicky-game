195125e39b336aee992105d1db89800c6a026398
crsimpson5 <crsimpson5@gmail.com>
2019-04-13 12:52:53 -0500

    Fixed images not loading

--- src/data/images.js
+++ src/data/images.js
@@ -1,19 +1,19 @@
 const images = [
-  { id: 0, src: "/images/bastion.PNG" },
-  { id: 1, src: "/images/dva.PNG" },
-  { id: 2, src: "/images/genji.PNG" },
-  { id: 3, src: "/images/lucio.PNG" },
-  { id: 4, src: "/images/mccree.PNG" },
-  { id: 5, src: "/images/mei.PNG" },
-  { id: 6, src: "/images/mercy.PNG" },
-  { id: 7, src: "/images/pharah.PNG" },
-  { id: 8, src: "/images/reaper.PNG" },
-  { id: 9, src: "/images/reinhardt.PNG" },
-  { id: 10, src: "/images/soldier.PNG" },
-  { id: 11, src: "/images/tracer.PNG" },
-  { id: 12, src: "/images/winston.PNG" },
-  { id: 13, src: "/images/zarya.PNG" },
-  { id: 14, src: "/images/zen.PNG" }
+  { id: 0, src: "/images/bastion.png" },
+  { id: 1, src: "/images/dva.png" },
+  { id: 2, src: "/images/genji.png" },
+  { id: 3, src: "/images/lucio.png" },
+  { id: 4, src: "/images/mccree.png" },
+  { id: 5, src: "/images/mei.png" },
+  { id: 6, src: "/images/mercy.png" },
+  { id: 7, src: "/images/pharah.png" },
+  { id: 8, src: "/images/reaper.png" },
+  { id: 9, src: "/images/reinhardt.png" },
+  { id: 10, src: "/images/soldier.png" },
+  { id: 11, src: "/images/tracer.png" },
+  { id: 12, src: "/images/winston.png" },
+  { id: 13, src: "/images/zarya.png" },
+  { id: 14, src: "/images/zen.png" }
 ]
 
 export default images;
